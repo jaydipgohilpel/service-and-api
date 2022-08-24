65074513cae1596fc1375cf236c883c1c5bc60f4
PE86PD\PEPD <jaydip.gohil@proexelancers.com>
2022-08-24 10:36:58 +0530

    routing and singlemobilerecord change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MobileSingleRecordComponent } from './mobile-single-record/mobile-single-record.component';
 import { HomeComponent } from './home/home.component';
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'singleMobileRecord', component: MobileSingleRecordComponent },
 ];
 
